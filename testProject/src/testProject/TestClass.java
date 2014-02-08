7cc5ee7963df0c6210368e1345ee9923b70e6df0
volcomrj <homeboy56@gmail.com>
2014-02-08 11:46:22 -0800

    v1.01

--- testProject/src/testProject/TestClass.java
+++ testProject/src/testProject/TestClass.java
@@ -1,8 +1,19 @@
 package testProject;
 
+import java.util.Random;
+
+
 public class TestClass {
 
+	
 	public static void main(String[] args){
-		System.out.println("Test Output");
+		for(int i=0; i <=10;i++)
+			System.out.println(getRandom());
+		
+	}
+	
+	public static int getRandom(){
+		Random random = new Random();
+		return random.nextInt()/100000000;
 	}
 }
